5c074c04c14fd29a43a51295886fd90fbc7810b7
ChinoBeach <beach988@morrisville.edu>
2022-03-01 02:22:32 -0500

    prevent double play

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -9,15 +9,19 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float fltLoadDelay = .5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
     void OnTriggerEnter2D(Collider2D other)
     {
         //If you bump your head
-        if(other.tag == "Ground")
+        if(other.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
             //if you hit your head(lose), restart the game, also make it so that you cant move anymore
             FindObjectOfType<PlayerController>().DisableControl();
-            
+
             crashEffect.Play();
+               
+            
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", fltLoadDelay);
         }
